8c7211ada9a0bc01d90bf03715f3ae5cd6ba7708
Trent <mr.peschke@gmail.com>
2020-08-01 09:15:39 -0600

    the Trait Any now gets replaced with a random trait

--- db/get/beastconflict.sql
+++ db/get/beastconflict.sql
@@ -1,1 +1,6 @@
-select * from bbconflict where beastid = $1 order by random()
+select id, value, beastid, type,
+REPLACE(trait, 'Any',  (SELECT trait FROM srdtraits
+    ORDER BY random()+bbconflict.id LIMIT 1)) as trait
+from bbconflict  
+where beastid = $1 
+order by random()
