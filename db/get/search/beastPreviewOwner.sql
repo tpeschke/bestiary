61a9cfe8761913a71017ece8123604a0d3757e31
Trent <mr.peschke@gmail.com>
2021-05-18 12:16:29 -0600

    redid search cards

--- db/get/search/beastPreviewOwner.sql
+++ db/get/search/beastPreviewOwner.sql
@@ -1,2 +1,2 @@
-select id, name, hr, intro, patreon, rarity, subsystem, int, size, canplayerview from bbindividualbeast
+select id, name, hr, intro, patreon, rarity, subsystem, int, size, canplayerview, thumbnail from bbindividualbeast
 where id = $1
