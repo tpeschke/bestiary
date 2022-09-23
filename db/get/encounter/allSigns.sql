a453017292cb9fd1c7e17f6176740168c5ad6f4a
Trent <mr.peschke@gmail.com>
2022-09-23 07:53:15 -0600

    bug squashing

--- db/get/encounter/allSigns.sql
+++ db/get/encounter/allSigns.sql
@@ -1,2 +1,11 @@
-select * from bbsigns
-where id not in (select signid from bbbeastsigns where beastid = $1)
+WITH added_row_number AS (
+  SELECT
+    id as signid, sign,
+    ROW_NUMBER() OVER(PARTITION BY sign ORDER BY id DESC) AS row_number
+  FROM bbsigns
+  where id not in (select signid from bbbeastsigns where beastid = $1)
+)
+SELECT
+  *
+FROM added_row_number
+WHERE row_number = 1;
