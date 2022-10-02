8fdc8416a8de3e6bacca7956566230250b8c1780
Trent <mr.peschke@gmail.com>
2022-10-02 07:11:57 -0600

    trying to set up meta data

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,18 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Meta } from '@angular/platform-browser';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  constructor() {  }
+export class AppComponent implements OnInit {
+  constructor(
+    private metaService: Meta
+  ) {  }
+
+  ngOnInit() {
+    this.metaService.updateTag( { name:'og:description', content: `The Bestiary for the Bonfire TTRPG`});
+    this.metaService.updateTag( { name:'og:image', content: "https://bestiary.dragon-slayer.net/assets/TWRealFire.png"});
+  }
 }
