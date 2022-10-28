42cab1f9800be17cc010367114c3adeb676f8dd6
Trent <mr.peschke@gmail.com>
2022-10-28 09:44:28 -0600

    bug squashing

--- db/get/beastconflict.sql
+++ db/get/beastconflict.sql
@@ -1,17 +1,17 @@
 select * from ( 
 (select id, value, beastid, type, socialroleid, allroles, trait from bbconflict  
-where beastid = 426 and type != 'h' and type != 'c')
+where beastid = $1 and type != 'h' and type != 'c')
 union 
 (select id, value, beastid, type, socialroleid, allroles,
 REPLACE(trait, 'Any', (SELECT conviction FROM srdconvictions s 
     ORDER BY random()+bbconflict.id LIMIT 1)) as trait
 from bbconflict  
-where beastid = 426 and type = 'c')
+where beastid = $1 and type = 'c')
 union
 (select id, value, beastid, type, socialroleid, allroles,
 REPLACE(trait, 'Any', (SELECT description FROM srddescriptions s 
     ORDER BY random()+bbconflict.id LIMIT 1)) as trait
 from bbconflict  
-where beastid = 426 and type = 'h')
+where beastid = $1 and type = 'h')
 ) t
 order by random()
