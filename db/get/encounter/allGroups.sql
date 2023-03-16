685e8230c33ca1572740454b3e7982ebcc3d476d
Trent <mr.peschke@gmail.com>
2023-03-16 08:45:05 -0600

    random encounter

--- db/get/encounter/allGroups.sql
+++ db/get/encounter/allGroups.sql
@@ -1,1 +1,2 @@
 select * from bbencounterlabels where beastid = $1
+order by weight desc, label
--- db/get/encounter/groupRoles.sql
+++ db/get/encounter/groupRoles.sql
@@ -1,1 +1,2 @@
-select * from bbroleweights where labelid = $1;
+select * from bbroleweights where labelid = $1
+order by weight desc, role;
