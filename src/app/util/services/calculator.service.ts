34cf8e5a42ef07463dd9ce20e0ebd27ea807d9ed
Trent <mr.peschke@gmail.com>
2023-07-09 18:34:58 -0600

    bug squashing

--- src/app/util/services/calculator.service.ts
+++ src/app/util/services/calculator.service.ts
@@ -9,30 +9,48 @@ export class CalculatorService {
   constructor() { }
 
   calculateAverageOfDice(diceString) {
-    let totalValue = 0
-    if (!diceString) {
-      return 0
-    }
-    diceString
-      .replace(/!| /g, '')
-      .split('+')
-      .forEach(val => {
-        if (val.includes('d')) {
-          if (val.includes('*')) {
-            val = val.split('*')
-            let dice = val[0].split('d')
-            dice[0] = val[1]
-            totalValue += Math.round((+dice[0] + (+dice[1] * dice[0])) / 2)
-          } else {
-            val = val.split('d')
-            val[0] = val[0] ? +val[0] : 1
-            totalValue += Math.round((val[0] + (+val[1] * val[0])) / 2)
+    if (typeof (diceString) === 'number') {
+      if (+diceString === 0) {
+        return 0
+      }
+      return +Math.floor(Math.random() * Math.floor(diceString)) + 1
+    } else {
+      let diceExpressionArray = []
+      let expressionValue = ""
+
+      diceString.replace(/\s/g, '').split('').forEach((val, i, array) => {
+        if (val === '-' || val === '+' || val === '*' || val === '/' || val === '(' || val === ')') {
+          diceExpressionArray.push(expressionValue)
+          if (i !== array.length - 1) {
+            diceExpressionArray.push(val)
           }
-        } else {
-          totalValue += +val
+          expressionValue = ""
+        }
+        if (!isNaN(+val) || val === 'd' || val === "!") {
+          val = val.replace(/!/i, "")
+          expressionValue = expressionValue + val;
+        }
+        
+        if (i === array.length - 1 && expressionValue !== '') {
+          diceExpressionArray.push(expressionValue);
         }
       })
-    return totalValue
+
+      for (let index = 0; index < diceExpressionArray.length; index++) {
+        let val = diceExpressionArray[index];
+
+        if (val.includes('d')) {
+          val = val.split('d')
+          let subtotal = 0
+          if (val[0] === "") { val[0] = 1 }
+          for (let i = 0; i < +val[0]; i++) {
+            subtotal += ( (+val[1] + 1) / 2 )
+          }
+          diceExpressionArray[index] = subtotal
+        }
+      }
+      return Math.ceil(evaluate(diceExpressionArray.join("")))
+    }
   }
 
   public rollDice = (diceString) => {
