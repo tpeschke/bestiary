0304b48938e597176f50fbeed71b51d42ef03d30
Trent <mr.peschke@gmail.com>
2023-08-16 14:57:51 -0600

    monster overhaul

--- db/update/combatStats.sql
+++ db/update/combatStats.sql
@@ -4,5 +4,5 @@ set beastid = $2, roleid = $3, piercingweapons = $4, slashingweapons = $5, crush
               rangeddefence = $13, alldefense = $14, allaround = $15, armorandshields = $16,
               unarmored = $17, attack = $18, isspecial = $19, eua = $20, addsizemod = $21, weapon = $22, shield = $23, 
               armor = $24, weaponname = $25, rangeddefense = $26, initiative = $27, measure = $28, recovery = $29, showonlydefenses = $30,
-              weapontype = $31
+              weapontype = $31, rangedistance = $32
 where id = $1
